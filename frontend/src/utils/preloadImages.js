29776440dab9f4fdcd9c4ae478dbdbb4a271fa78
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2024-09-14 14:16:43 +0200

    fixing

--- frontend/src/utils/preloadImages.js
+++ frontend/src/utils/preloadImages.js
@@ -1,11 +1,18 @@
+import Djokovic__01__edit from '../assets/Djokovic__01__edit.jpg'
+import Djokovic__01 from '../assets/Djokovic__01.jpg'
+import Swiatek__01__edit from '../assets/Swiatek__01__edit.jpg'
+import Swiatek__01 from '../assets/Swiatek__01.jpg'
+import LightBackground from '../assets/pattern__seamless__light.png'
+import DarkBackground from '../assets/pattern__seamless__dark.png'
+
 const preloadImages = () => {
     const images = [
-      '../assets/Djokovic__01__edit.jpg',
-      '../assets/Djokovic__01.jpg',
-      '../assets/Swiatek__01__edit.jpg',
-      '../assets/Swiatek__01.jpg',
-      '../assets/pattern__seamless__light.png',
-      '../assets/pattern__seamless__dark.png'
+      Djokovic__01__edit,
+      Djokovic__01,
+      Swiatek__01__edit,
+      Swiatek__01,
+      LightBackground,
+      DarkBackground
     ];
   
     images.forEach(src => {
